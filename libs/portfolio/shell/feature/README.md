37bb3f39ee0bd753e6de25ac44d8cfbc5b65f816
Frederic Lammens <flammens@ctg.com>
2022-10-14 17:21:55 +0200

    feat(libs): add shell component with routing enabled

---
+++ libs/portfolio/shell/feature/README.md
@@ -0,0 +1,7 @@
+# portfolio-shell-feature
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test portfolio-shell-feature` to execute the unit tests.
---
+++ libs/portfolio/shell/feature/src/lib/portfolio-shell.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { PorfolioShellRoutingModule } from './porfolio-shell-routing.module';
+
+@NgModule({
+  imports: [PorfolioShellRoutingModule],
+})
+export class PortfolioShellModule {}
