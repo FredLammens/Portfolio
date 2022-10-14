6aa23f2c04aea0f2e0849b8100d9661a1278ba22
Frederic Lammens <flammens@ctg.com>
2022-10-14 14:04:03 +0200

    Initial commit

---
+++ apps/portfolio/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fred-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'portfolio';
+}
