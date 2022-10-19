02b6d2901384abda9f13e063915516ccbc6ca6da
Frederic Lammens <flammens@ctg.com>
2022-10-19 14:19:33 +0200

    refactor(navigation): add singlepage routing support

--- libs/portfolio/shell/ui/navigation/src/lib/navigation/navigation.component.ts
+++ libs/portfolio/shell/ui/navigation/src/lib/navigation/navigation.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
+//change to chared folder
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -7,5 +10,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavigationComponent {
-  public constructor() {}
+  //id's from components to scroll to
+  @Input() public singlePageAnchors: Array<string> = ['Home', 'About', 'Work', 'Skills', 'Contact'];
+  public readonly navroutes = this.router.config;
+  public linkClass = '';
+
+  public constructor(private router: Router, private scroller: ViewportScroller) {}
+
+  public scrollTo(anchor: string): void {
+    this.scroller.scrollToAnchor(anchor);
+  }
 }
