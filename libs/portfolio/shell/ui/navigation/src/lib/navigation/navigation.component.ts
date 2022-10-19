111626b2aec5d6ad98bd069025ce2c3eaae273fb
Frederic Lammens <flammens@ctg.com>
2022-10-19 16:51:05 +0200

    refactor(navigation): remove more code xd

--- libs/portfolio/shell/ui/navigation/src/lib/navigation/navigation.component.ts
+++ libs/portfolio/shell/ui/navigation/src/lib/navigation/navigation.component.ts
@@ -1,10 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-//TODO: change to shared folder
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavigationComponent {
--- libs/portfolio/shell/ui/navigation/src/lib/navigation/navigationAnchor.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Anchor {
-  anchor: string;
-  toggled: boolean;
-}
