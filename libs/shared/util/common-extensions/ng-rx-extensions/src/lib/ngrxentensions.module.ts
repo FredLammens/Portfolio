7d823a57137503cb79c6d8c9a1b78157c0fda11e
Frederic Lammens <flammens@ctg.com>
2022-10-25 16:01:14 +0200

    feat(emoji): add emoji support

--- libs/shared/util/common-extensions/ng-rx-extensions/src/lib/ngrxentensions.module.ts
+++ libs/shared/util/common-extensions/ng-rx-extensions/src/lib/ngrxentensions.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { EmojiDirective } from './emoji.directive';
 import { IntersectDirective } from './intersect.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [IntersectDirective],
-  exports: [IntersectDirective],
+  declarations: [IntersectDirective, EmojiDirective],
+  exports: [IntersectDirective, EmojiDirective],
 })
 export class NgrxentensionsModule {}
