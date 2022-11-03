e2016415dc00e74bc7d7b77469825bb23530246a
Frederic Lammens <flammens@ctg.com>
2022-11-03 11:51:08 +0100

    style(board): mobile is carousel with animations

--- libs/portfolio/shell/feature/src/lib/portfolio-shell.module.ts
+++ libs/portfolio/shell/feature/src/lib/portfolio-shell.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationModule } from '@fred/portfolio/shell/ui/navigation';
 
 import { ShellRoutes } from './shell.routing';
 
 @NgModule({
-  imports: [ShellRoutes, NavigationModule],
+  imports: [ShellRoutes, NavigationModule, BrowserAnimationsModule],
 })
 export class PortfolioShellModule {}
--- libs/shared/ui/src/lib/board/board.component.ts
+++ libs/shared/ui/src/lib/board/board.component.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 import { Project, State } from '../models/project.model';
@@ -6,12 +7,25 @@ import { Project, State } from '../models/project.model';
   selector: 'kanban-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
+  animations: [
+    trigger('easeInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('.3s ease-out', style({ transform: 'translateX(0%)' })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0%)' }),
+        animate('.3s ease-out', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class BoardComponent {
   public projectsDone: Array<Project> = [];
   public projectsBacklog: Array<Project> = [];
   public projectsInProgress: Array<Project> = [];
 
+  //TODO: add check if mobile to put done to true rest to false
   public boards = {
     backlog: true,
     inProgress: true,
