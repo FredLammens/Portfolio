44ff6f2d36c2a8aaa8c87c58dc557993b2405c49
Frederic Lammens <flammens@ctg.com>
2022-11-03 11:11:15 +0100

    feat(board): made board into carousel when on mobile

--- libs/shared/ui/src/lib/board/board.component.ts
+++ libs/shared/ui/src/lib/board/board.component.ts
@@ -11,6 +11,13 @@ export class BoardComponent {
   public projectsDone: Array<Project> = [];
   public projectsBacklog: Array<Project> = [];
   public projectsInProgress: Array<Project> = [];
+
+  public boards = {
+    backlog: true,
+    inProgress: true,
+    done: true,
+  };
+
   @Input()
   public set projects(projs: Array<Project>) {
     this.projectsBacklog = [];
@@ -21,5 +28,26 @@ export class BoardComponent {
       if (proj.state === State.InProgess) this.projectsInProgress.push(proj);
       if (proj.state === State.Done) this.projectsDone.push(proj);
     });
+  }
+
+  public switch(board: string) {
+    if (board === 'backlog') {
+      this.boards.backlog = true;
+      this.boards.inProgress = false;
+      this.boards.done = false;
+      return;
+    }
+    if (board === 'inProgress') {
+      this.boards.backlog = false;
+      this.boards.inProgress = true;
+      this.boards.done = false;
+      return;
+    }
+    if (board === 'done') {
+      this.boards.backlog = false;
+      this.boards.inProgress = false;
+      this.boards.done = true;
+      return;
+    }
   }
 }
