803a6433fe9a56908768331e8623e238a714a0d1
Frederic Lammens <flammens@ctg.com>
2023-01-05 15:41:47 +0100

    chore(project-work-card): add basic work card structure

--- libs/shared/ui/src/lib/project-work-card/project-work-card.component.ts
+++ libs/shared/ui/src/lib/project-work-card/project-work-card.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'project-work-card',
   templateUrl: './project-work-card.component.html',
-  styleUrls: ['./project-work-card.component.css'],
+  styleUrls: ['./project-work-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectWorkCardComponent {
