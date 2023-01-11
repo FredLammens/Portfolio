04f6b5fcd42a24ebfdf736210d83a85fe6ef54db
Frederic Lammens <flammens@ctg.com>
2023-01-11 15:36:57 +0100

    chore(project-work-card): add reverse css

--- libs/shared/ui/src/lib/project-work-card/project-work-card.component.ts
+++ libs/shared/ui/src/lib/project-work-card/project-work-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'project-work-card',
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectWorkCardComponent {
-  constructor() {}
+  @Input() public isReverse = false;
 }
